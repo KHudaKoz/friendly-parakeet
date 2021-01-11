b8b6da512e75032343cb62fa4f6a18f366f1fbd0
Kip HudaKoz <kip.hudakoz@me.com>
2021-01-10 21:14:50 -0500

    Final Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,22 @@ generateButton.addEventListener('click', function () {
 
 // GOT TO START THE PROCESS OF CREATING. INPUT THROUG FUNCTION ONCE BUTTON CLICKED PROMPT WITH NUMBER RANGE 8 -128 USING FUNCTION WITH VAR AND NEST IF 
 function generateNewPassword() {
-  var length = Number(prompt("Please select the desired charactor length within the new password.  Select a number between 8-128."));
+  var length = Number(prompt("Please select the desired charactor length for your new password.  Select a number between 8-128."));
   while (Number.isNaN(length) || length < 8 || length > 128) length = Number(prompt("You must select a number no-less than 8 but no more than 128 characters."));
 
   // VARIBLES WITH CONFIRMS BO0LEANS T || F : uppercaseletters, lowercaseletters< NUMBERS, SYMBOLS
-  var numbers = confirm("Should numbers be included in the new password?  OK = YES, and CANCLE = NO.");
-  var symbols = confirm("Should special characters be included in the new password? OK = YES, and CANCLE = NO.");
-  var lowercaseletters = confirm("Should lowercase letters be included in your password? OK = YES, and CANCLE = NO.");
-  var uppercaseletters = confirm("Should UPPERCASE letters be included in your password? OK = YES, and CANCLE = NO.");
+  var numbers = confirm("Should numbers be included in your new password?  OK = YES, and CANCLE = NO.");
+  var symbols = confirm("Should special characters be included in your new password? OK = YES, and CANCLE = NO.");
+  var lowercaseletters = confirm("Should lowercase letters be included in your new password? OK = YES, and CANCLE = NO.");
+  var uppercaseletters = confirm("Should UPPERCASE letters be included in your new password? OK = YES, and CANCLE = NO.");
 
   // CONFIRM INCLUDE QESTIONS IF THE DON'T SELECT>
   while (!uppercaseletters && !lowercaseletters && !numbers && !symbols) {
     alert("You fail to select at least one character type! Try again.");
-    numbers = confirm("Should numbers be included in the new password?  OK = YES, and CANCLE = NO.");
-    symbols = confirm("Should special characters be included in the new password? OK = YES, and CANCLE = NO.");
-    lowercaseletters = confirm("Should lowercase letters be included in your password? OK = YES, and CANCLE = NO.");
-    uppercaseletters = confirm("Should UPPERCASE letters be included in your password? OK = YES, and CANCLE = NO.");
+    numbers = confirm("Should numbers be included in your new password?  OK = YES, and CANCLE = NO.");
+    symbols = confirm("Should special characters be included in your new password? OK = YES, and CANCLE = NO.");
+    lowercaseletters = confirm("Should lowercase letters be included in your new password? OK = YES, and CANCLE = NO.");
+    uppercaseletters = confirm("Should UPPERCASE letters be included in your new password? OK = YES, and CANCLE = NO.");
     
   }
 
